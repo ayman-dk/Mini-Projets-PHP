18445cd7fb1d7286099e85f526bc07966700786b
ayman-dk <aymanedaki02@gmail.com>
2026-02-01 18:49:16 +0100

    Succes

---
+++ 01-gestion-finances/db.php
@@ -0,0 +1,15 @@
+<?php
+$host = 'localhost';
+$dbname = 'mp_gestion_finances';
+$username = 'root';
+$password = '';
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connexion réussie à la base de données.";
+}
+catch (PDOException $e){
+    die("Erreur de connexion : " . $e->getMessage());
+}
+?>
