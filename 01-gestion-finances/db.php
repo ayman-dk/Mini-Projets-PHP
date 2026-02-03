f10779c9f000f24c14e638cb6b26e299afc17583
ayman-dk <aymanedaki02@gmail.com>
2026-02-03 16:04:06 +0100

    Succes

--- 01-gestion-finances/db.php
+++ 01-gestion-finances/db.php
@@ -7,6 +7,7 @@ $password = '';
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo->exec("set names utf8mb4");
 }
 catch (PDOException $e){
     die("Erreur de connexion : " . $e->getMessage());
