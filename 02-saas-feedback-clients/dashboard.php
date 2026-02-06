292a6e4f7715271b3c496829296e30e3b3b36e8e
ayman-dk <aymanedaki02@gmail.com>
2026-02-06 13:42:23 +0100

    Succes

--- 02-saas-feedback-clients/dashboard.php
+++ 02-saas-feedback-clients/dashboard.php
@@ -1,9 +1,12 @@
 <?php
  session_start();
+ require 'includes/db.php';
  // Si la session n'existe pas, on renvoie vers le login
     if(!isset($_SESSION['user_id'])){
         header('location: login.php');
         exit();
     }
     echo "Bienvenue sur votre dashboard, " . $_SESSION['user_email'] . " !";
+
+    $sql = "SELECT api_key FROM users WHERE id = :id";
     ?>
